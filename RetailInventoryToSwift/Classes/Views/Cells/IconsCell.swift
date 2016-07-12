5bbb582cf392312996fd84772071e2b724b3d8ba
Victor Belyavsky <v.belyavsky@itransition.com>
2016-07-12 11:36:04 +0300

    - icon border

--- RetailInventoryToSwift/Classes/Views/Cells/IconsCell.swift
+++ RetailInventoryToSwift/Classes/Views/Cells/IconsCell.swift
@@ -13,11 +13,19 @@ class IconsCell: UICollectionViewCell {
     static let cellIdentifier = String(IconsCell)
     
     @IBOutlet private weak var icon: UILabel!
+    @IBOutlet private weak var borderView: UIView!
     
     var title: Character? {
         didSet {            
             icon.font = UIFont(name: "FontAwesome", size: MyConstant.iconSize)
             icon.text = String(title!)
         }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        borderView.layer.borderColor = UIColor.whiteColor().CGColor
+        borderView.layer.borderWidth = 1
     }
 }
